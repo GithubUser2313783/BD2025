083952547e6096765dcc84705a2f5d453c97586a
Ian-Christopher Lazo-Sojo <2313783@collegemv.qc.ca>
2025-05-15 16:01:54 -0400

    FinalPractice

--- TD12_-_Revision_-_Enonce.sql
+++ TD12_-_Revision_-_Enonce.sql
@@ -1,119 +1,119 @@
 -- ---------------------------------------------------------
--- Collège Marie Victorin
--- Introduction aux bases de données
--- Révision
+-- Collï¿½ge Marie Victorin
+-- Introduction aux bases de donnï¿½es
+-- Rï¿½vision
 -- ---------------------------------------------------------
 
--- Série I :  les conditions (Clause WHERE)
--- Base de données : xtremefr
+-- Sï¿½rie I :  les conditions (Clause WHERE)
+-- Base de donnï¿½es : xtremefr
 
----- Créez une requête qui affiche les employés nés les dix derneirs jours du mois
--- Les colonnes à afficher sont : 
--- •	Nom
--- •	Prénom
--- •	Poste occupé
--- •	Date de naissance
--- •	Téléphone personnel
+---- Crï¿½ez une requï¿½te qui affiche les employï¿½s nï¿½s les dix derneirs jours du mois
+-- Les colonnes ï¿½ afficher sont : 
+-- ï¿½	Nom
+-- ï¿½	Prï¿½nom
+-- ï¿½	Poste occupï¿½
+-- ï¿½	Date de naissance
+-- ï¿½	Tï¿½lï¿½phone personnel
 
 SELECT [Nom],
-	   [Prénom],
-	   [Poste Occupé],
+	   [Prï¿½nom],
+	   [Poste Occupï¿½],
 	   [Date de naissance],
-	   [Téléphone personnel]
-FROM [dbo].[Employés]
+	   [Tï¿½lï¿½phone personnel]
+FROM [dbo].[Employï¿½s]
 WHERE DAY([Date de naissance]) BETWEEN 20 AND 31
 
 
----- Créez une requête qui affiche les employés embauché le premier mois de l'année.
--- Les colonnes à afficher sont : 
--- •	Nom
--- •	Prénom
--- •	Poste occupé
--- •	Date d'embauche
--- •	Téléphone personnel
+---- Crï¿½ez une requï¿½te qui affiche les employï¿½s embauchï¿½ le premier mois de l'annï¿½e.
+-- Les colonnes ï¿½ afficher sont : 
+-- ï¿½	Nom
+-- ï¿½	Prï¿½nom
+-- ï¿½	Poste occupï¿½
+-- ï¿½	Date d'embauche
+-- ï¿½	Tï¿½lï¿½phone personnel
 
 SELECT [Nom],
-	   [Prénom],
-	   [Poste Occupé],
+	   [Prï¿½nom],
+	   [Poste Occupï¿½],
 	   [Date d'embauche],
-	   [Téléphone personnel]
-FROM [dbo].[Employés]
+	   [Tï¿½lï¿½phone personnel]
+FROM [dbo].[Employï¿½s]
 WHERE MONTH([Date d'embauche]) LIKE 1
 
----- Créez une requête qui affiche les employés qui sont des directeurs
--- Les colonnes à afficher sont : 
--- •	Nom
--- •	Prénom
--- •	Poste occupé
--- •	Téléphone personnel
+---- Crï¿½ez une requï¿½te qui affiche les employï¿½s qui sont des directeurs
+-- Les colonnes ï¿½ afficher sont : 
+-- ï¿½	Nom
+-- ï¿½	Prï¿½nom
+-- ï¿½	Poste occupï¿½
+-- ï¿½	Tï¿½lï¿½phone personnel
 
 SELECT [Nom],
-	   [Prénom],
-	   [Poste Occupé],
-	   [Téléphone personnel]
-FROM [dbo].[Employés]
-WHERE [Poste occupé] LIKE 'Directeur%'
+	   [Prï¿½nom],
+	   [Poste Occupï¿½],
+	   [Tï¿½lï¿½phone personnel]
+FROM [dbo].[Employï¿½s]
+WHERE [Poste occupï¿½] LIKE 'Directeur%'
 
 
----- Créez une requête qui affiche la liste des représentants ainsi que leur age et leur ancienneté
--- Les colonnes à afficher sont : 
--- •	Nom
--- •	Prénom
--- •	Poste occupé
--- •	Téléphone personnel
--- •	Age
--- •	Ancienneté
+---- Crï¿½ez une requï¿½te qui affiche la liste des reprï¿½sentants ainsi que leur age et leur anciennetï¿½
+-- Les colonnes ï¿½ afficher sont : 
+-- ï¿½	Nom
+-- ï¿½	Prï¿½nom
+-- ï¿½	Poste occupï¿½
+-- ï¿½	Tï¿½lï¿½phone personnel
+-- ï¿½	Age
+-- ï¿½	Anciennetï¿½
 
 SELECT [Nom],
-	   [Prénom],
-	   [Poste Occupé],
-	   [Téléphone personnel],
+	   [Prï¿½nom],
+	   [Poste Occupï¿½],
+	   [Tï¿½lï¿½phone personnel],
 	   DATEDIFF(YEAR, [Date de naissance], GETDATE()) Age,
-	   DATEDIFF(YEAR, [Date d'embauche], GETDATE()) Ancienneté
-FROM [dbo].[Employés]
+	   DATEDIFF(YEAR, [Date d'embauche], GETDATE()) Anciennetï¿½
+FROM [dbo].[Employï¿½s]
 
----- Créez une requête qui affiche la liste des employés qui ont pour contact en cas d'urgence le père ou la mère
--- Les colonnes à afficher sont : 
--- •	Nom
--- •	Prénom
--- •	Poste occupé
--- •	Téléphone personnel
--- •	Lien de parenté
-
-SELECT [Nom],
-	   [Prénom],
-	   [Poste Occupé],
-	   [Téléphone personnel],
-	   [En cas d'urgence - Lien de parenté]
-FROM [dbo].[Employés]
-WHERE [En cas d'urgence - Lien de parenté] IN ('Père', 'Mère')
-
----- Créez une requête qui affiche la liste des employés qui relèvent de l'employé qui n'a pas de supérieur hiérarchique.
--- Les colonnes à afficher sont : 
--- •	Nom
--- •	Prénom
--- •	Poste occupé
--- •	Supérieur hiérarchique
+---- Crï¿½ez une requï¿½te qui affiche la liste des employï¿½s qui ont pour contact en cas d'urgence le pï¿½re ou la mï¿½re
+-- Les colonnes ï¿½ afficher sont : 
+-- ï¿½	Nom
+-- ï¿½	Prï¿½nom
+-- ï¿½	Poste occupï¿½
+-- ï¿½	Tï¿½lï¿½phone personnel
+-- ï¿½	Lien de parentï¿½
 
 SELECT [Nom],
-	   [Prénom],
-	   [Poste Occupé],
-	   [Supérieur hiérarchique]
-FROM [dbo].[Employés]
-WHERE [Supérieur hiérarchique] IS NULL
+	   [Prï¿½nom],
+	   [Poste Occupï¿½],
+	   [Tï¿½lï¿½phone personnel],
+	   [En cas d'urgence - Lien de parentï¿½]
+FROM [dbo].[Employï¿½s]
+WHERE [En cas d'urgence - Lien de parentï¿½] IN ('Pï¿½re', 'Mï¿½re')
+
+---- Crï¿½ez une requï¿½te qui affiche la liste des employï¿½s qui relï¿½vent de l'employï¿½ qui n'a pas de supï¿½rieur hiï¿½rarchique.
+-- Les colonnes ï¿½ afficher sont : 
+-- ï¿½	Nom
+-- ï¿½	Prï¿½nom
+-- ï¿½	Poste occupï¿½
+-- ï¿½	Supï¿½rieur hiï¿½rarchique
+
+SELECT [Nom],
+	   [Prï¿½nom],
+	   [Poste Occupï¿½],
+	   [Supï¿½rieur hiï¿½rarchique]
+FROM [dbo].[Employï¿½s]
+WHERE [Supï¿½rieur hiï¿½rarchique] IS NULL
 
 -- ------------------------------------------------------------------------------------------------------------------------
 -- Sderie II
 
--- Afficher la liste des clients ayant acheté des produits valant plus de 500$
+-- Afficher la liste des clients ayant achetï¿½ des produits valant plus de 500$
 -- On affichera :
 -- Le nom du client
--- La catégorie
+-- La catï¿½gorie
 -- Le nom du produit
 -- Le prix unitaire
--- La quantité
+-- La quantitï¿½
 -- La date de la commande
--- La liste sera triée par le nom du client et le nom du produit.
+-- La liste sera triï¿½e par le nom du client et le nom du produit.
 
 -- Votre code ici
 
@@ -121,38 +121,38 @@ SELECT [Nom du client],
 	   [Nom du type de produit],
 	   [Nom du produit],
 	   [Prix unitaire],
-	   [Quantité],
+	   [Quantitï¿½],
 	   [Date de commande]
 FROM [dbo].[Clients]
 JOIN [dbo].[Commandes] ON [dbo].[Commandes].[ID client] = [dbo].[Clients].[ID client]
-JOIN [dbo].[Détails des commandes] ON [dbo].[Détails des commandes].[ID commande] = [dbo].[Commandes].[ID commande]
-JOIN [dbo].[Produits] ON [dbo].[Produits].[ID produit] = [dbo].[Détails des commandes].[ID produit]
+JOIN [dbo].[Dï¿½tails des commandes] ON [dbo].[Dï¿½tails des commandes].[ID commande] = [dbo].[Commandes].[ID commande]
+JOIN [dbo].[Produits] ON [dbo].[Produits].[ID produit] = [dbo].[Dï¿½tails des commandes].[ID produit]
 JOIN [dbo].[Types de produit] ON [dbo].[Types de produit].[ID type de produit] = [dbo].[Produits].[ID type de produit]
-WHERE [Prix unitaire] * [Quantité] > 500
+WHERE [Prix unitaire] * [Quantitï¿½] > 500
 ORDER BY [Nom du client], [Nom du produit]
 
 
 
 -- -----------------------------------------------------------------
--- Créer une vue qui contient les données de la requête précédente
+-- Crï¿½er une vue qui contient les donnï¿½es de la requï¿½te prï¿½cï¿½dente
 -- -----------------------------------------------------------------
 -- Votre code ici
 
 
 -- -------------------------------------------------------------------------------------------------------------------------------
--- Maintenant, on veut savoir combien de fois chaque client a acheté le même produit ainsi que la quantité totale et coût total de ce produit
+-- Maintenant, on veut savoir combien de fois chaque client a achetï¿½ le mï¿½me produit ainsi que la quantitï¿½ totale et coï¿½t total de ce produit
 -- On affichera :
 -- Le nom du client
 -- Le nom du produit
--- Le nombre de fois que le produit a été acheté
--- La quantité totale achetée
--- Le montant total acheté
+-- Le nombre de fois que le produit a ï¿½tï¿½ achetï¿½
+-- La quantitï¿½ totale achetï¿½e
+-- Le montant total achetï¿½
 
 -- Votre code ici
 
 
 -- --------------------------------------------------------------------------
--- Créer la table tblStatsClients contenant les données calculées plus haut
+-- Crï¿½er la table tblStatsClients contenant les donnï¿½es calculï¿½es plus haut
 
 -- Votre code ici
 
